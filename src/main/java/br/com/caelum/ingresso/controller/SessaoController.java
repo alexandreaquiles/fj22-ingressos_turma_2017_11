0f906a919be9dc1c195adf263f4eadfcc853e2ca
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-12-02 10:40:55 -0200

    form com RequestMethod.GET

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
@@ -18,7 +19,7 @@ public class SessaoController {
 	@Autowired
 	private FilmeDao filmeDao;
 
-	@RequestMapping("/admin/sessao")
+	@RequestMapping(value="/admin/sessao", method=RequestMethod.GET)
 	public String form(@RequestParam("salaId") Integer salaId, Model model) {
 		model.addAttribute("sala", salaDao.findOne(salaId));
 		model.addAttribute("filmes", filmeDao.findAll());
