31f687b0b9429dc9154b360fe276e9987fca38a3
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-12-02 10:50:11 -0200

    Usando ModelAndView no SessaoController

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
@@ -19,10 +20,11 @@ public class SessaoController {
 	private FilmeDao filmeDao;
 
 	@GetMapping("/admin/sessao")
-	public String form(@RequestParam("salaId") Integer salaId, Model model) {
-		model.addAttribute("sala", salaDao.findOne(salaId));
-		model.addAttribute("filmes", filmeDao.findAll());
-		return "sessao/sessao";
+	public ModelAndView form(@RequestParam("salaId") Integer salaId) {
+		ModelAndView mav = new ModelAndView("sessao/sessao");
+		mav.addObject("sala", salaDao.findOne(salaId));
+		mav.addObject("filmes", filmeDao.findAll());
+		return mav;
 	}
 }
 
