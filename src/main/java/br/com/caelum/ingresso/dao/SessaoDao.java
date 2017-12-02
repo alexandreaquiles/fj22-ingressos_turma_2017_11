0539e5a6534cd637a35659c2a421d1e047e0e2d3
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-12-02 12:17:09 -0200

    Mudando salva no SessaoDao pra usar @Transactional

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -1,10 +1,10 @@
 package br.com.caelum.ingresso.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.caelum.ingresso.model.Sessao;
 
@@ -14,23 +14,9 @@ public class SessaoDao {
 	@PersistenceContext
 	private EntityManager manager;
 
+	@Transactional
 	public void save(Sessao sessao) {
-		EntityTransaction tx = manager.getTransaction();
-		tx.begin();
-		
-		try {
-			manager.persist(sessao);
-			tx.commit();
-			
-		} catch(Exception ex) {
-			tx.rollback();
-			throw new RuntimeException("Erro ao inserir sessão.", ex);
-		}
+		manager.persist(sessao);
 	}
-	
-	
-	
-	
-	
 
 }
